e7630ab9a8b1ce95716e13c1464b3be7bac88a36
Paulo Jânio dos Santos Júnior <janiosantos@mobit.com.br>
2025-07-10 14:32:13 -0300

    NLP data loader concluded. It basically takes an input and passes it in the DataLoader class which has the collate_function. The collate_function tokenizes the sequence and converts the newly generated tokens into vocabulary, creating tensors. Then finally applies padding to the batches, when necessary.

--- nlpdataloader/src/nlp_data_loader.py
+++ nlpdataloader/src/nlp_data_loader.py
@@ -26,23 +26,44 @@ sentences = [
 
 # Define a custom dataset.
 class CustomDataset(Dataset):
-    def __init__(self, sentences, tokenizer, vocab):
+    def __init__(self, sentences):
         self.sentences = sentences
-        self.tokenizer = tokenizer
-        self.vocab = vocab
     
     def __len__(self):
         return len(self.sentences)
 
     def __getitem__(self, idx):
-        tokens = self.tokenizer(self.sentences[idx])
-        tensor_indices = [self.vocab[token] for token in tokens]
-        return torch.tensor(tensor_indices)
+        return self.sentences[idx]
 
-# Create a custom collate function.
-def collate_fn(batch):
+# Collate function with batch_first=False (default).
+def collate_fn_bfFALSE(batch):
+    # Tokenize each sample in the batch using the specified tokenizer.
+    tensor_batch = []
+    for sample in batch:
+        tokens = tokenizer(sample)
+        # Convert tokens to vocabulary indics and create tensor.
+        tensor_batch.append(torch.tensor([vocab[token] for token in tokens]))
+
     # Pad sequences within the batch to have equal lengths.
-    padded_batch = pad_sequence(batch, batch_first=True, padding_value=0)
+    # Without specifying a value for batch_first it goes default to False, ensuring that the tensors have shape (max_sequence_length ,batch_size)
+    padded_batch = pad_sequence(tensor_batch)
+    
+    # Return padded batch.
+    return padded_batch
+
+def collate_fn(batch):
+    # Tokenize each sample in the batch using the specified tokenizer.
+    tensor_batch = []
+    for sample in batch:
+        tokens = tokenizer(sample)
+        # Convert tokens to vocabulary indices and create a tensor for each sample.
+        tensor_batch.append(torch.tensor([vocab[token] for token in tokens]))
+
+    # Pad sequences within the batch to have equal lengths using pad_sequence.
+    # batch_first=True ensures that the tensors have shape (batch_size, max_sequence_length).
+    padded_batch = pad_sequence(tensor_batch, batch_first=True)
+    
+    # Return the padded batch.
     return padded_batch
 
 # Tokenizer.
@@ -52,21 +73,64 @@ tokenizer = get_tokenizer("basic_english")
 vocab = build_vocab_from_iterator(map(tokenizer, sentences))
 
 # Create an instance of your custom data set.
-custom_dataset = CustomDataset(sentences, tokenizer, vocab)
+custom_dataset = CustomDataset(sentences)
 
 # Define a batch size.
 batch_size = 2
 
-# Create a data loader.
-dataloader = DataLoader(custom_dataset, batch_size=batch_size, collate_fn = collate_fn,shuffle=True)
+# Data loader with collate_function_bfFALSE.
+dataloader_bfFALSE = DataLoader(
+    dataset=custom_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn = collate_fn_bfFALSE
+)
 
-# Iterate through the data loader
+# Data loader with collate_function.
+dataloader = DataLoader(
+    dataset=custom_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    collate_fn=collate_fn
+)
+
+# Iterate through the data loader with batch_first=True and printing batches
+print("Iterate through the data loader with batch_first=True and printing batches")
+for batch in dataloader:    
+    print(batch)
+    print("Length of sequences in the batch:",batch.shape[1])
+
+print("---------------------------------------------")
+
+# Iterate through the data loader with batch_first=True and printing vocab
+print("Iterate through the data loader with batch_first=True and printing vocab")
 for batch in dataloader:
     for row in batch:
         for idx in row:
             words = [vocab.get_itos()[idx] for idx in row]
         print(words)
 
+print("---------------------------------------------")
+
+# Iterate through the data loader with batch_first=False and printing batches
+print("Iterate through the data loader with batch_first=False and printing batches")
+for batch in dataloader_bfFALSE:    
+    print(batch)
+    print("Length of sequences in the batch:",batch.shape[1])
+
+print("---------------------------------------------")
+
+# Iterate through the data loader with batch_first=False and printing vocab
+print("Iterate through the data loader with batch_first=False and printing vocab")
+for batch in dataloader_bfFALSE:
+    for row in batch:
+        for idx in row:
+            words = [vocab.get_itos()[idx] for idx in row]
+        print(words)
+
+print("---------------------------------------------")
+
+print("Note that as we made all transformations inside the collate_function, the original dataset remains untouched.")
 print("Custom Dataset Length:", len(custom_dataset))
 print("Sample Items:")
 for i in range(6):
